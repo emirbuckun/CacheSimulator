3adfd4694bb1561a7d58d6e1b523895f6e128e1d
Cem Bohan <cembohan@gmail.com>
2023-06-07 15:55:27 +0300

    file operations added

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.PrintWriter;
 public class App {
     public static void main(String[] args) throws Exception {
         int setIndex = 0, linesPerSet = 0, blockBits = 0;
         String traceFile = "";
-
+        
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-s":
@@ -28,5 +36,56 @@ public class App {
         System.out.println("Lines Per Set: " + linesPerSet);
         System.out.println("Block Bits: " + blockBits);
         System.out.println("Trace file: " + traceFile);
+        
+        FileReader reader = new FileReader("..\\traces\\"+traceFile);
+    	int data = reader.read();
+    	ArrayList<String> traceLine = new ArrayList<>();
+    	traceLine.add("");
+    	int traceTemp=0;
+    	while(data!=-1) {
+    		//we read one line at a time, only saving one line in the same array at a time. access the line elements in the else block.
+    		if(data==' ') {
+    			traceTemp++;
+    			data=reader.read();
+    			traceLine.add("");
+    		}	
+    		else if(data==',') {
+    			data=reader.read();
+    		}
+    		else if(Character.isAlphabetic(data)||Character.isDigit(data)) {
+    			traceLine.set(traceTemp, traceLine.get(traceTemp).concat(""+(char)data));
+    			data=reader.read();
+    		}
+    		else {
+    			for(int i = 0; i<traceLine.size();i++) {
+    				System.out.print(traceLine.get(i)+" ");
+    			}
+    			System.out.println();
+    			traceLine.clear();
+    			traceLine.add("");
+    			traceTemp=0;
+    			data=reader.read();
+    		}
+    	}
+    	reader.close();
+    	readRam();
+    }
+    public static void readRam() throws IOException {
+    	PrintWriter writer = new PrintWriter("..\\Ram.txt");
+    	FileInputStream reader = new FileInputStream("..\\RAM.dat");
+    	DataInputStream in = new DataInputStream(reader);
+    	int data;
+    	byte[] b= {,};
+    	boolean eof=false;
+    	while(!eof) {
+    		try {
+				data=in.readShort();
+//				System.out.print(Integer.toHexString(data)+" ");
+				writer.write(Integer.toHexString(data)+" ");
+			} catch (EOFException e) {
+				eof=true;
+			}
+    		
+    	}
     }
 }
