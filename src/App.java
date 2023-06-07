aef6ed8162af841aae8eb059b76a0a4bf20b9857
Emir Buckun <emirbuckun@hotmail.com>
2023-06-07 07:53:26 +0300

    Argument reading and printing added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int setIndex = 0, linesPerSet = 0, blockBits = 0;
+        String traceFile = "";
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-s":
+                    setIndex = Integer.parseInt(args[++i]);
+                    break;
+                case "-E":
+                    linesPerSet = Integer.parseInt(args[++i]);
+                    break;
+                case "-b":
+                    blockBits = Integer.parseInt(args[++i]);
+                    break;
+                case "-t":
+                    traceFile = args[++i];
+                    break;
+                default:
+                    System.out.println("Invalid argument: " + args[i]);
+                    System.exit(1);
+            }
+        }
+
+        // Now you have the parameters, you can print them to check
+        System.out.println("Set Index: " + setIndex);
+        System.out.println("Lines Per Set: " + linesPerSet);
+        System.out.println("Block Bits: " + blockBits);
+        System.out.println("Trace file: " + traceFile);
     }
 }
